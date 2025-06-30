ac134018cd74f05141a8efa651118628fe22567b
Neeraj Thakur <97724487+sou1nonly@users.noreply.github.com>
2025-06-30 11:15:10 +0530

    Delete apps/auth_service/src/routes/profileRoutes.ts

--- apps/auth_service/src/routes/profileRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authenticateJWT } from "../middleware/authMiddleware"; 
-const router = Router();
-
-router.get("/profile", authenticateJWT, (req, res) => {
-  res.json({ message: `Welcome user ${req.user?.user_id}` });
-});
-
-export default router;
