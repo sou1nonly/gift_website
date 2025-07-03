7306cba8aca45e89944206afd717ecc5bc6be2f1
Neeraj Thakur <97724487+sou1nonly@users.noreply.github.com>
2025-07-03 14:32:27 +0530

    Delete apps/auth_service directory

--- apps/auth_service/src/utils/customError.ts
+++
@@ -1,9 +0,0 @@
-export class CustomError extends Error {
-  statusCode: number;
-
-  constructor(message: string, statusCode = 500) {
-    super(message);
-    this.statusCode = statusCode;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
